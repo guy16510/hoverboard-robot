62aa49ea9a392ef155e177f69609369725b4c8f4
Chris Burns <burns.christopherd@gmail.com>
2026-07-23 17:39:53 -0400

    Run SWD UART timing tests

--- tests/native/test_main.c
+++ tests/native/test_main.c
@@ -8,12 +8,14 @@ void gs_test_protocol(void);
 void gs_test_control(void);
 void gs_test_architecture(void);
 void gs_test_simulation(void);
+void gs_test_swd_timing(void);
 
 int main(void) {
   gs_test_protocol();
   gs_test_control();
   gs_test_architecture();
   gs_test_simulation();
+  gs_test_swd_timing();
   if (gs_tests_failed != 0) {
     fprintf(stderr, "%u/%u assertions failed\n", gs_tests_failed, gs_tests_run);
     return 1;

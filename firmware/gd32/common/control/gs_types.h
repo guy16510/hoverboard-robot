4f50b89f87fd7b9bbab746104016f9fddc640050
Chris Burns <burns.christopherd@gmail.com>
2026-07-23 13:18:38 -0400

    Add coherent runtime telemetry types

--- firmware/gd32/common/control/gs_types.h
+++ firmware/gd32/common/control/gs_types.h
@@ -29,4 +29,38 @@ typedef struct {
   bool bridge_enabled;
 } gs_motor_demand;
 
+typedef enum {
+  GS_RUNTIME_BRIDGE_ENABLED = 1u << 0,
+  GS_RUNTIME_PA4_RAW_HIGH = 1u << 1,
+  GS_RUNTIME_PA4_BYPASS = 1u << 2,
+  GS_RUNTIME_ADC_VALID = 1u << 3,
+  GS_RUNTIME_ADC_READY = 1u << 4,
+  GS_RUNTIME_ADC_IN_RANGE = 1u << 5,
+  GS_RUNTIME_HALL_VALID = 1u << 6,
+} gs_runtime_flag;
+
+typedef struct {
+  uint32_t rx_bytes;
+  uint32_t tx_bytes;
+  uint32_t rx_overflows;
+  uint32_t tx_overflows;
+  uint32_t framing_errors;
+} gs_transport_telemetry;
+
+typedef struct {
+  int16_t requested_command;
+  int16_t demanded_command;
+  int16_t applied_command;
+  uint16_t compare_offset;
+  uint32_t hall_interval_us;
+  uint32_t hall_capture_overflows;
+  uint16_t adc_value;
+  uint16_t adc_baseline;
+  uint8_t motor_state;
+  uint8_t flags;
+  uint8_t hall;
+  uint8_t previous_hall;
+  uint8_t adc_samples;
+} gs_runtime_telemetry;
+
 #endif

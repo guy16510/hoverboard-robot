87c37bd68c332dd10afe349d874a31f9538bcf4f
Chris Burns <burns.christopherd@gmail.com>
2026-07-23 20:50:10 -0400

    Run sequence restart recovery tests

--- tests/native/test_main.c
+++ tests/native/test_main.c
@@ -9,6 +9,7 @@ void gs_test_control(void);
 void gs_test_architecture(void);
 void gs_test_simulation(void);
 void gs_test_swd_timing(void);
+void gs_test_resync(void);
 
 int main(void) {
   gs_test_protocol();
@@ -16,6 +17,7 @@ int main(void) {
   gs_test_architecture();
   gs_test_simulation();
   gs_test_swd_timing();
+  gs_test_resync();
   if (gs_tests_failed != 0) {
     fprintf(stderr, "%u/%u assertions failed\n", gs_tests_failed, gs_tests_run);
     return 1;

671eb236517c86b1daf2aa0a59bda1540ec0e5f8
Chris Burns <burns.christopherd@gmail.com>
2026-07-26 16:16:08 -0400

    Defer service install until customization passes

--- raspberry-pi-image/stage-trashcan/00-install/00-run.sh
+++ raspberry-pi-image/stage-trashcan/00-install/00-run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -e
 
+set -euo pipefail
+
 install -d -m 0755 "${ROOTFS_DIR}/opt/trashcan-robot"
 rsync -a --delete "${STAGE_DIR}/00-install/files/repository/" "${ROOTFS_DIR}/opt/trashcan-robot/"
-install -d -m 0755 "${ROOTFS_DIR}/etc/systemd/system"
-install -m 0644 \
-  "${ROOTFS_DIR}/opt/trashcan-robot/donkeycar/systemd/trashcan-donkeycar.service" \
-  "${ROOTFS_DIR}/etc/systemd/system/trashcan-donkeycar.service"
